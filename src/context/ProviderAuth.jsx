8ce4329ae7fcbda6e5562c79a7d897d0254247f3
joshua360x <jkw5499@psu.edu>
2022-05-07 16:59:49 -0400

    built out context to manage state globally

---
+++ src/context/ProviderAuth.jsx
@@ -0,0 +1,26 @@
+import React, { createContext, useState } from 'react'
+
+export const contextAuth = createContext()
+
+export default function ProviderAuth({ children }) {
+  const [userRecieved, setUserRecived] = useState(null)
+
+  function loginUser(email, password) {
+    const successfulLoginOfUser = email === 'jo@jow.com' && password === 'password'
+
+    successfulLoginOfUser ?? setUserRecived({ email })
+    return successfulLoginOfUser
+  }
+
+  function logOutUser(callbackFuntion) {
+    setUserRecived(null)
+    callbackFuntion()
+  }
+
+
+  return (
+    <contextAuth.Provider value={{ userRecieved, logOutUser, loginUser }}>
+      {children}
+    </contextAuth.Provider>
+  )
+}
